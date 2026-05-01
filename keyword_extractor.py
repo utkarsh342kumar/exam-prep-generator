1cb9ef6c56f304211fb900f640afb0013bba9c9c
utkarsh342kumar <yeeshu93678@gmail.com>
2026-05-01 23:51:27 +0530

    Refactor keyword extraction to load model once

--- keyword_extractor.py
+++ keyword_extractor.py
@@ -1,19 +1,11 @@
 import spacy
 
+nlp = spacy.load("en_core_web_sm")
+
 def extract_keywords(text):
-    try:
-        nlp = spacy.load("en_core_web_sm")
-    except:
-        from spacy.cli import download
-        download("en_core_web_sm")
-        nlp = spacy.load("en_core_web_sm")
-
     doc = nlp(text)
-
     keywords = []
-
     for token in doc:
         if token.pos_ in ["NOUN", "PROPN"]:
             keywords.append(token.text)
-
     return list(set(keywords))
